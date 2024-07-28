6b003db2ac7a66925791b3f290ea98473c69a2de
Pranav R <pranav81234@gmail.com>
2024-07-28 15:24:00 +0530

    initiated collaboration tools

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -8,10 +9,18 @@ import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider 
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({userIds})
+
+        return users
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
 };
 
 export default Provider;
+ 
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,15 +4,19 @@ import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
 import Header from "./Header";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Editor } from "./editor/Editor";
+import ActiveCollaborators from "./ActiveCollaborators";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({ roomId, roomMetadata } : CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         <div className="collaborative-room">
           <Header>
             <div className="flex w-fit items-center justify-center gap-2">
               <p className="document-title">Share</p>
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
             </div>
             <SignedOut>
               <SignInButton />
